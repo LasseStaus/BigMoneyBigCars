f001d86488c6b4b8ff57f247845e15db91929f86
Lasse <L.stausgaard@gmail.com>
2020-04-07 00:14:21 +0200

    Slider works for desktop with pictures, needs adjustment per image,  Need to be  rechanged to textboxes

--- main.js
+++ main.js
@@ -12,6 +12,70 @@ function start() {
   fetchJson();
 }
 
+const slider = document.querySelector(".slider");
+const buttons = document.querySelectorAll(".btn");
+const options = document.querySelectorAll(".option");
+const slides = document.querySelectorAll(".img");
+
+let index = 1;
+let op_index = 0;
+let size = slides[index].clientWidth;
+
+update();
+function update() {
+  slider.style.transform = "translateX(" + -size * index + "px)";
+  options.forEach(op => op.classList.remove("colored"));
+  options[op_index].classList.add("colored");
+}
+
+function slide() {
+  slider.style.transition = "transform .5s ease-in-out";
+  update();
+}
+
+function btnCheck() {
+  console.log(index);
+  if (this.id === "prev") {
+    index--;
+    if (op_index == 4) {
+      op_index = 4;
+    } else {
+      op_index--;
+    }
+    console.log(index);
+  } else if (this.id === "next") {
+    console.log(index);
+    index++;
+    if (op_index == 4) {
+      op_index = 0;
+    } else {
+      op_index++;
+    }
+  }
+  slide();
+}
+function optionFunc() {
+  let i = Number(this.getAttribute("option-index"));
+  index = i + 1;
+  op_index = i;
+  slide();
+}
+
+slider.addEventListener("transitionend", () => {
+  if (slides[index].id === "last") {
+    slider.style.transition = "none";
+    index = slides.length - 2;
+    slider.style.transform = "translateX(" + -size * index + "px)";
+  } else if (slides[index].id === "first") {
+    slider.style.transition = "none";
+    index = 1;
+    slider.style.transform = "translateX(" + -size * index + "px)";
+  }
+});
+
+buttons.forEach(btn => btn.addEventListener("click", btnCheck));
+options.forEach(option => option.addEventListener("click", optionFunc));
+
 const toggler = document.querySelector(".menu__toggler");
 const menu = document.querySelector(".mobile-menu-dropdown");
 const atags = document.querySelector(".atags-mobile-menu");
