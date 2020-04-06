ced1f3ba54239af04f1fe44a3ff2b8d55c590116
Lasse <L.stausgaard@gmail.com>
2020-04-06 21:34:05 +0200

    stage before branch with new slider

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
 "use strict";
+
+import "@babel/polyfill";
 window.addEventListener("DOMContentLoaded", start);
 
-import { gallery, scrolling } from "./modules/functions";
+import { gallery, scrolling, projectElement, gallery2 } from "./modules/functions";
 
-const data = [
-  {
-    i: "download.png"
-  },
-  {
-    i: "download.png"
-  }
-];
+function start() {
+  scrolling();
+  document.querySelector(".the-container").addEventListener("scroll", scrolling);
+  //document.querySelector(".next").addEventListener("click", plusSlides);
+  fetchJson();
+}
 
 const toggler = document.querySelector(".menu__toggler");
 const menu = document.querySelector(".mobile-menu-dropdown");
 const atags = document.querySelector(".atags-mobile-menu");
 const mAtags = document.querySelectorAll(".atags-mobile-menu > a");
 
-console.log(mAtags);
 toggler.addEventListener("click", () => {
   toggler.classList.toggle("active");
   document.querySelector(".the-container").classList.toggle("blurred");
@@ -29,20 +28,59 @@ toggler.addEventListener("click", () => {
 mAtags.forEach(a => {
   a.addEventListener("click", () => {
     document.querySelector(".the-container").classList.remove("blurred");
-    menu.classList.remove("shadow");
+
     atags.classList.toggle("fade-in-left");
     toggler.classList.toggle("active");
     menu.classList.toggle("active");
+    menu.classList.remove("shadow");
   });
 });
 
-gallery(data, document.querySelector("#section_2"));
-
 //when scrolling happens, call scrolling()
 
-function start() {
-  scrolling();
-  document.querySelector(".the-container").addEventListener("scroll", scrolling);
+async function fetchJson() {
+  let response = await fetch("test.json");
+  let jsonData = await response.json();
+
+  tis(jsonData);
+}
+
+function tis(jsonData) {
+  console.log(jsonData);
+  let p1 = jsonData.projects.project1;
+  let p2 = jsonData.projects.project2;
+  let p3 = jsonData.projects.project3;
+  let p4 = jsonData.projects.project4;
+
+  console.log(p1);
+  console.log(p2);
+  console.log(p3);
+  console.log(p4);
 }
 
 //max scroll : scrollheight - clientheight
+
+/* const data2 = [
+  {
+    i: "download.png"
+  },
+  {
+    i: "download.png"
+  },
+  {
+    i: "download.png"
+  },
+  {
+    i: "download.png"
+  }
+];
+
+let p = document.querySelector("#projects .container");
+console.log(p);
+
+projectElement(data2, p); */
+
+/* test.forEach(t => {
+  const e = document.createElement("div");
+  gallery(data2, e);
+}); */
--- modules/functions.js
+++ modules/functions.js
@@ -1,21 +1,55 @@
 export function gallery(data, parent) {
   data.forEach(d => {
-    const t = document.createElement("img");
+    const t = createElement("img");
     t.src = "imgs/" + d.i;
+    console.log(d.i);
     parent.appendChild(t);
   });
 }
-
 function createElement(type) {
   return document.createElement(type);
 }
+/* 
+export function gallery2(data2, e) {
+  data2.forEach(d => {
+    const t = createElement("img");
+    t.src = "imgs/" + d.i;
+    console.log(d.i);
+    return e.appendChild(t);
+  });
+}
+
+
+
+export function projectElement(data2, p) {
+  data2.forEach(d => {
+    const e = createElement("div");
+    p.appendChild(e);
+    e.classList.add("project");
+    console.log(e);
+    gallery2(data2, e);
+    console.log(p);
+  });
+} */
+
+/* export function projectElement(data, parent, type, clas) {
+  console.log(clas);
+
+  data.forEach(d => {
+    console.log("project element");
+    const e = createElement("div");
+    e.classlist.add("${clas}");
+
+    parent.appendChild(e);
+  });
+} */
 
 export function scrolling() {
   console.log("scrolling");
   let cnt = document.querySelector(".the-container");
   let ratio = cnt.scrollTop / (cnt.scrollHeight - cnt.clientHeight);
   let scrollinfo = document.querySelector("#scrollinfo");
-  // console.log(scrollinfo);
+  //console.log(scrollinfo);
 
   scrollinfo.style.setProperty("--ratio", ratio);
 
